c863624f4591f07428b1bd05068e0f48a2644274
Edward Ardian Tayu <edwardtayu4@gmail.com>
2023-04-28 13:34:51 +0700

    Menambahkan SQLiteDatabaseHandler, TaskHandler, dan layout menu untuk menambahkan task.

--- app/src/main/java/com/example/finalproject1/MainActivity.java
+++ app/src/main/java/com/example/finalproject1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.finalproject1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabaseHandler sqLiteDatabaseHandler;
+
+    private ListView ListTugas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sqLiteDatabaseHandler = new SQLiteDatabaseHandler(this);
+        ListTugas = (ListView) findViewById(R.id.list_tugas);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
